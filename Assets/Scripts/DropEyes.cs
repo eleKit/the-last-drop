6df7dda8a3cecb2c9e8517fb4efd9206a4a31739
elekit <kit93@hotmail.it>
2016-12-27 09:31:03 +0100

    Added Drop Eyes

---
+++ Assets/Scripts/DropEyes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropEyes : MonoBehaviour
+{
+
+	public GameObject Drop;
+
+	Transform tr;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		tr = gameObject.GetComponent<Transform> ();
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		tr.position = Drop.gameObject.transform.position;
+		
+	}
+}
