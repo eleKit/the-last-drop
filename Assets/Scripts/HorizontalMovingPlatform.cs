55893c62c35ae217303ecc129fa344c1321cc445
elekit <kit93@hotmail.it>
2016-12-29 12:18:01 +0100

    Horizontal moving platform script and prefab

---
+++ Assets/Scripts/HorizontalMovingPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovingPlatform : MonoBehaviour
+{
+
+	Transform tr;
+
+	// Use this for initialization
+	void Start ()
+	{
+		tr = gameObject.GetComponent<Transform> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		tr.position = new Vector3 (Mathf.PingPong (Time.time, 3), tr.position.y, tr.position.z);
+		
+	}
+}
