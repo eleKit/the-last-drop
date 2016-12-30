e474f6c24b6dbebec1f579c02621d7b1dd471aa9
elekit <kit93@hotmail.it>
2016-12-30 11:41:23 +0100

    Falling platform script and prefab

---
+++ Assets/Scripts/DisappearPlatform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearPlatform : MonoBehaviour
+{
+	[Range (0f, 4f)]
+	public float fallDelay = 1f;
+
+	private Rigidbody2D rb2d;
+	private Collider2D coll2d;
+
+
+	void Awake ()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		coll2d = GetComponent<Collider2D> ();
+	}
+
+
+
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			Invoke ("Fall", fallDelay);
+		}
+
+	}
+
+	void Fall ()
+	{
+		rb2d.isKinematic = false;
+		Destroy (coll2d);
+		StartCoroutine (WaitBeforeDisappear ());
+	}
+
+
+	IEnumerator WaitBeforeDisappear ()
+	{
+		yield return new WaitForSeconds (2.5f);
+		gameObject.SetActive (false);
+	}
+}
