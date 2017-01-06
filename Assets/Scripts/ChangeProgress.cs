e12a4215074ace8ef484365487ded275ad1e9c63
elekit <kit93@hotmail.it>
2017-01-06 11:17:30 +0100

    Reset progress and Cheat buttons added in Main Menu Settings

---
+++ Assets/Scripts/ChangeProgress.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeProgress : MonoBehaviour
+{
+	//Remember you are working with an array, so count from zero!!
+	private const int MaxLevel = 9;
+
+	public void ResetProgress ()
+	{
+		PlayerPrefs.DeleteKey ("LastWonLevel");
+	}
+
+
+	public void Cheat ()
+	{
+		PlayerPrefs.SetInt ("LastWonLevel", MaxLevel);
+	}
+}
