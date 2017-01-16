ed25302df49e15d94b5d19e9eb15c3b3f37e81cd
elekit <kit93@hotmail.it>
2017-01-16 22:01:38 +0100

    Color all the background plants at the end of the level

---
+++ Assets/Scripts/ColorTrees.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTrees : MonoBehaviour
+{
+
+	[Header ("Wood part of the tree")]
+	public SpriteRenderer[] wood;
+
+	[Header ("Leafs part of the tree")]
+	public SpriteRenderer[] leaf;
+
+		
+
+	public void ColorTree ()
+	{
+		
+		//color the leafs
+		for (int i = 0; i < leaf.Length; i++) {
+			leaf [i].color = new Color32 (202, 244, 82, 255);
+			Debug.Log ("Color the leaf" + leaf [i].color.ToString ());
+		}
+
+		// color the woody parts of the tree
+		for (int i = 0; i < wood.Length; i++) {
+			wood [i].color = new Color32 (238, 207, 168, 255);  
+		}
+	}
+}
--- Assets/Scripts/FirstPrototype_scripts/WinPoint.cs
+++ Assets/Scripts/FirstPrototype_scripts/WinPoint.cs
@@ -6,6 +6,8 @@ public class WinPoint : MonoBehaviour
 {
 	public GameObject m_plant_animation;
 
+	public ColorTrees m_trees_coloured;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -26,6 +28,8 @@ public class WinPoint : MonoBehaviour
 		GameObject go = POLIMIGameCollective.ObjectPoolingManager.Instance.GetObject (m_plant_animation.name);
 		go.transform.position = this.gameObject.transform.position;
 		go.transform.rotation = this.gameObject.transform.rotation;
+
+		m_trees_coloured.ColorTree ();
 		//TODO SoundManager.Instance.WinGingle ();
 		gameObject.SetActive (false);
 		GameWinManager.Instance.WinLevel ();
